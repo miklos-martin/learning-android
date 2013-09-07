087a8cf873b4ca5c858b21eae33234d8c966a714
Martin Miklós <martin.miklos@1nd.hu>
2013-09-07 16:25:23 +0200

    splash screen

---
+++ LearningAndroid/src/main/java/miklos/martin/learningandroid/Splash.java
@@ -0,0 +1,39 @@
+package miklos.martin.learningandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Splash screen
+ */
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timer = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch ( InterruptedException e ) {
+                    e.printStackTrace();
+                } finally {
+                    Intent openMain = new Intent( "miklos.martin.learningandroid.MAINACTIVITY" );
+                    startActivity( openMain );
+                }
+            }
+        };
+
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
