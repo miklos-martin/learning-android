823af88c6280bb124c546b18dfc0952a8a6e6e82
Martin Miklós <martin.miklos@1nd.hu>
2013-09-07 16:01:40 +0200

    playing with buttons

--- LearningAndroid/src/main/java/miklos/martin/learningandroid/MainActivity.java
+++ LearningAndroid/src/main/java/miklos/martin/learningandroid/MainActivity.java
@@ -3,15 +3,42 @@ package miklos.martin.learningandroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    int counter;
+    Button add, sub;
+    TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        counter = 0;
+        add = (Button) findViewById( R.id.bAdd );
+        sub = (Button) findViewById( R.id.bSub );
+        display = (TextView) findViewById( R.id.tvDisplay );
+
+        add.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                display.setText( "Total: " + counter );
+            }
+        });
+
+        sub.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter--;
+                display.setText( "Total: " + counter );
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +46,4 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-This repo follows [these tutorials][thenewboston]
+This repo follows [these tutorials][thenewboston] more or less.
 
 [thenewboston]: https://www.youtube.com/watch?v=SUOWNXGRc6g&feature=c4-overview-vl&list=PL2F07DBCDCC01493A
