fe921455aae37bdddf0d169d806420caa29ebb1f
Martin Miklós <martin.miklos@1nd.hu>
2013-09-07 22:22:15 +0200

    alignment, color

--- LearningAndroid/src/main/java/miklos/martin/learningandroid/TextPlay.java
+++ LearningAndroid/src/main/java/miklos/martin/learningandroid/TextPlay.java
@@ -1,8 +1,10 @@
 package miklos.martin.learningandroid;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,9 +22,10 @@ public class TextPlay extends Activity {
         setContentView( R.layout.text );
 
         final EditText input = (EditText) findViewById( R.id.etCommands );
-        Button checkCommand = (Button) findViewById( R.id.bResults );
         final ToggleButton passTogg = (ToggleButton) findViewById( R.id.tbResults );
-        TextView display = (TextView) findViewById( R.id.tvResults );
+        final TextView display = (TextView) findViewById( R.id.tvResults );
+
+        Button checkCommand = (Button) findViewById( R.id.bResults );
 
         passTogg.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -31,6 +34,28 @@ public class TextPlay extends Activity {
                     input.setInputType( InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD );
                 } else {
                     input.setInputType( InputType.TYPE_CLASS_TEXT );
+                }
+            }
+        });
+
+        checkCommand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String check = input.getText().toString();
+                if ( check.contentEquals("left") ) {
+                    display.setGravity( Gravity.LEFT );
+                    display.setText( "left" );
+                } else if ( check.contentEquals("center") ) {
+                    display.setGravity( Gravity.CENTER );
+                    display.setText( "center" );
+                } else if ( check.contentEquals( "right" ) ) {
+                    display.setGravity( Gravity.RIGHT );
+                    display.setText( "right" );
+                } else if ( check.contentEquals( "blue" ) ) {
+                    display.setTextColor(Color.BLUE);
+                    display.setText( "blue" );
+                } else {
+                    display.setText( "Invalid" );
                 }
             }
         });
