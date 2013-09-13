2ca7ec6de7b5fbffa3ee16a6ac6f14d8de72c9ba
Martin Miklós <martin.miklos@1nd.hu>
2013-09-13 08:05:50 +0200

    make splash fullscreen

--- LearningAndroid/src/main/java/miklos/martin/learningandroid/Splash.java
+++ LearningAndroid/src/main/java/miklos/martin/learningandroid/Splash.java
@@ -3,6 +3,8 @@ package miklos.martin.learningandroid;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Splash screen
@@ -12,7 +14,16 @@ public class Splash extends Activity {
     @Override
     protected void onCreate ( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
+
+        // make it fullscreen
+        requestWindowFeature( Window.FEATURE_NO_TITLE );
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
         setContentView( R.layout.splash );
+
 
         Thread timer = new Thread() {
             @Override
