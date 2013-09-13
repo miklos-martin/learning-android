264ced642faa069a5fc260e326da50272ab7539b
Martin Miklós <martin.miklos@1nd.hu>
2013-09-13 18:02:53 +0200

    surfaceview basic thread

--- LearningAndroid/src/main/java/miklos/martin/learningandroid/AnimatedSurfaceView.java
+++ LearningAndroid/src/main/java/miklos/martin/learningandroid/AnimatedSurfaceView.java
@@ -1,6 +1,8 @@
 package miklos.martin.learningandroid;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -11,6 +13,7 @@ public class AnimatedSurfaceView extends SurfaceView implements Runnable {
 
     SurfaceHolder holder;
     Thread animation = null;
+    boolean isRunning = true;
 
     public AnimatedSurfaceView ( Context context ) {
         super( context );
@@ -23,5 +26,15 @@ public class AnimatedSurfaceView extends SurfaceView implements Runnable {
     @Override
     public void run () {
 
+        while ( isRunning ) {
+
+            if ( !holder.getSurface().isValid() ) {
+                continue;
+            }
+
+            Canvas canvas = holder.lockCanvas();
+            canvas.drawColor( Color.CYAN );
+            holder.unlockCanvasAndPost( canvas );
+        }
     }
 }
