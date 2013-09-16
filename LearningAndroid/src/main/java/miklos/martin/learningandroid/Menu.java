c3e59d15193b2bd206e06c70ddc8fa2b182364e9
Martin Miklós <martin.miklos@1nd.hu>
2013-09-16 17:37:03 +0200

    sliding drawer

--- LearningAndroid/src/main/java/miklos/martin/learningandroid/Menu.java
+++ LearningAndroid/src/main/java/miklos/martin/learningandroid/Menu.java
@@ -13,7 +13,11 @@ import android.widget.ListView;
  */
 public class Menu extends ListActivity {
 
-    String classes[] = { "MainActivity", "TextPlay", "Email", "Camera", "Data", "Graphics", "GraphicsSurface", "SoundStuff" };
+    String classes[] = {
+            "MainActivity", "TextPlay", "Email", "Camera", "Data",
+            "Graphics", "GraphicsSurface", "SoundStuff",
+            "Slider"
+    };
 
     @Override
     protected void onCreate ( Bundle savedInstanceState ) {
---
+++ LearningAndroid/src/main/java/miklos/martin/learningandroid/Slider.java
@@ -0,0 +1,15 @@
+package miklos.martin.learningandroid;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import java.util.HashMap;
+
+public class Slider extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.slider);
+    }
+}
