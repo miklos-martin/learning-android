709c656eeb30e0cdd088a5e099d6c062c0b6fe8c
Martin Miklós <martin.miklos@1nd.hu>
2013-09-16 17:49:29 +0200

    sliding drawer methods

--- LearningAndroid/src/main/java/miklos/martin/learningandroid/Slider.java
+++ LearningAndroid/src/main/java/miklos/martin/learningandroid/Slider.java
@@ -1,15 +1,75 @@
 package miklos.martin.learningandroid;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.SlidingDrawer;
 
-import java.util.HashMap;
+public class Slider extends Activity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, SlidingDrawer.OnDrawerOpenListener {
 
-public class Slider extends Activity {
-
+    Button handle1, handle2, handle3, handle4;
+    CheckBox checkBox;
+    SlidingDrawer drawer;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.slider);
+        
+        initialize();
+    }
+
+    private void initialize () {
+        handle1 = (Button) findViewById( R.id.handle1 );
+        handle2 = (Button) findViewById( R.id.handle2 );
+        handle3 = (Button) findViewById( R.id.handle3 );
+        handle4 = (Button) findViewById( R.id.handle4 );
+        checkBox = (CheckBox) findViewById( R.id.cbSlideable );
+        drawer = (SlidingDrawer) findViewById( R.id.slidingDrawer );
+
+        handle1.setOnClickListener( this );
+        handle2.setOnClickListener( this );
+        handle3.setOnClickListener( this );
+        handle4.setOnClickListener( this );
+        checkBox.setOnCheckedChangeListener( this );
+        drawer.setOnDrawerOpenListener( this );
+    }
+
+    @Override
+    public void onClick ( View view ) {
+
+        switch ( view.getId() ) {
+            case R.id.handle1:
+                drawer.open();
+                break;
+            case R.id.handle2:
+                break;
+            case R.id.handle3:
+                drawer.toggle();
+                break;
+            case R.id.handle4:
+                drawer.close();
+                break;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged ( CompoundButton compoundButton, boolean b ) {
+
+        if ( compoundButton.isChecked() ) {
+            drawer.lock();
+        } else {
+            drawer.unlock();
+        }
+    }
+
+    @Override
+    public void onDrawerOpened () {
+        MediaPlayer mp = MediaPlayer.create( this, R.raw.explosion );
+        mp.start();
     }
 }
