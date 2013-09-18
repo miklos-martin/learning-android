7eb1b2b266b82cf638447f10b87670a16018144f
Martin Miklós <martin.miklos@1nd.hu>
2013-09-18 07:57:33 +0200

    view flipper

---
+++ LearningAndroid/src/main/java/miklos/martin/learningandroid/Flipper.java
@@ -0,0 +1,34 @@
+package miklos.martin.learningandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ViewFlipper;
+
+/**
+ * ViewFlipper example
+ */
+public class Flipper extends Activity implements View.OnClickListener {
+
+    ViewFlipper flipper;
+
+    @Override
+    protected void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.flipper );
+
+        initialize();
+    }
+
+    private void initialize () {
+
+        flipper = (ViewFlipper) findViewById( R.id.vfFlipper );
+
+        flipper.setOnClickListener( this );
+    }
+
+    @Override
+    public void onClick ( View view ) {
+        flipper.showNext();
+    }
+}
